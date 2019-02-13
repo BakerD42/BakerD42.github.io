93cdae59bc01b61af57246094bbcf9911256190c
BakerD42 <bak12027@byui.edu>
2019-02-13 12:51:34 -0700

    lesson 6 stuff

--- lesson-6/scripts/currentdate.js
+++ lesson-6/scripts/currentdate.js
@@ -1,3 +1,23 @@
 //Create a self updating copyright date
 var date = new Date();
 document.getElementById('copyright-year').innerHTML = date.getFullYear();
+
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+
+
+
+var monthName = new Array(12);
+month[0] = "January";
+
+
+var fullyear = today.getFullYear();
+
+
+
+console.log(today);
+
+
+var fulldate = weekday + ", " +
+    today.getdate() + " " + 
+    monthName + " " fullyear;
