476b47eef9238500de044a187956807843b8513c
BakerD42 <bak12027@byui.edu>
2019-02-23 20:25:50 -0700

    getting closer with grid stuff

--- lesson-7/scripts/windspeed.js
+++ lesson-7/scripts/windspeed.js
@@ -1,10 +1,7 @@
 var temp = 50;
-let windChill = document.getElementById("windChill").innerHTML;
-
-let result = parseInt((35.74 + 0.6215 *
-temp - 35.75 * Math.pow(windSpeed, 0.16) 
-+ 0.4275 * temp * Math.pow(windSpeed, 0.16)));
+var windSpeed = 10;
+var windChill = (35.74 + 0.6215 * temp - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temp * Math.pow(windSpeed, 0.16));
 
 document.getElementById("temp").innerHTML = temp + '&deg;';
-document.getElementById("windSpeed").innerHTML = windSpeed + ' &mph;';
+document.getElementById("windSpeed").innerHTML = windSpeed + ' mph;';
 document.getElementById("windChill").innerHTML = windChill + '&deg;';
