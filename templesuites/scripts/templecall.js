48997df3d7b8ab81ea7477597a7e9a4a8ce4f3a1
BakerD42 <bak12027@byui.edu>
2019-04-08 15:23:32 -0600

    json call test

--- templesuites/scripts/templecall.js
+++ templesuites/scripts/templecall.js
@@ -1,12 +1,13 @@
 let templeRequest = new XMLHttpRequest();
 templeRequest.open ('GET', "../templesuites/json/temples.json",true);
 templeRequest.send();
+//retrieve temple JSON data
 
 
 templeRequest.onload = function () {
     var templeData = JSON.parse(templeRequest.responseText);
     console.log(templeData);
 
-    document.getElementById("denverdescription").innerHTML = denverdescription;
+    document.getElementById("denverdescription").innerHTML = templeData.denverdescription;
     // name containers and place containters in various areas of site page
 }
