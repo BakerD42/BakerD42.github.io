693be858c4b428354df70dbbdf63fa1132322976
BakerD42 <bak12027@byui.edu>
2019-04-10 12:38:45 -0600

    JSON now works. trying to populate in correct area

--- templesuites/scripts/templecall.js
+++ templesuites/scripts/templecall.js
@@ -11,33 +11,34 @@ templeRequest.send();
 
 templeRequest.onload = function () {
     var templeData = templeRequest.response;
+    console.log(templeData);
     showTemples(templeData);
 }
 
 function showTemples(jsonObj) {
-    var templeData = jsonObj['temples'];
+    var templeData = jsonObj;
 
-    for (var i = 0; i < templeData; i++) {
-        if (i === 0 || i === 1 || i === 2 || i === 3 || i === 4){
-            continue;
-        }
-    
+    // for (var i = 0; i < templeData; i++) {
+    //     if (i === 0 || i === 1 || i === 2 || i === 3 || i === 4){
+    //         continue;
+    //     }
+    var i = 1;
 
-    var myName = document.createElement('name');
-    var myLocate = document.createElement('location');
+    var myName = document.createElement('div');
+    var myLocate = document.createElement('div');
     myLocate.className += "templeLocate";
-    var myClosures = document.createElement('closures');
+    var myClosures = document.createElement('ul');
     myClosures.className += "templeClosure";
-    var myServices = document.createElement('services');
+    var myServices = document.createElement('ul');
     myServices.className += "templeService";
-    var myDescription = document.createElement('description');
+    var myDescription = document.createElement('div');
     myDescription.className += "templeDescription";
 
-    myName.textContent = templeDatai[i].myName;
-    myLocate.textContent = templeData[i].myLocate;
-    myClosures.textContent = templeData[i].myClosures;
-    myServices.textContent = templeData[i].myServices;
-    myDescription.textContent = templeData[i].myDescription;
+    myName.textContent = templeData[i].name;
+    myLocate.textContent = templeData[i].location;
+    myClosures.textContent = templeData[i].closures;
+    myServices.textContent = templeData[i].services;
+    myDescription.textContent = templeData[i].description;
 
      var services = templeData[i].services;
      for (var j = 0; j < services.length; j++) {
@@ -53,6 +54,6 @@ myName.appendChild(myDescription);
 
 section.appendChild(myName);
 }
-}
+
 
 
