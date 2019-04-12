3d79badfe2b1ec64b214fbd617436b7dca3a4170
BakerD42 <bak12027@byui.edu>
2019-04-11 18:13:49 -0600

    added weather scripts and updates to each page

---
+++ templesuites/scripts/rexburgweather.js
@@ -0,0 +1,11 @@
+let weatherRequest = new XMLHttpRequest();
+weatherRequest.open ('GET', '//api.openweathermap.org/data/2.5/weather?zip=83440,us&appid=7a7bd0cc92fa44b75a7c863f9ba055a4&units=imperial',true);
+weatherRequest.send();
+
+
+weatherRequest.onload = function () {
+    var weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+
+    document.getElementById("current-temp").innerHTML = weatherData.main.temp;
+}
